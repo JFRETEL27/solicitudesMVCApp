72ca71e0510f91b1a966afc1d2e25fd1bb9adfd2
JF <josefretel24@gmail.com>
2022-12-01 17:44:03 -0500

    Init
    Init MVC App + SQL Server

---
+++ SolicitudesMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SolicitudesMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
